e0385965bf749bfc48d6f8d1b46d6f7655020514
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2014-11-19 14:16:46 +0000

    Removed gsp resource module as it does not work with asset pipeline.

--- app/grails-app/conf/BuildConfig.groovy
+++ app/grails-app/conf/BuildConfig.groovy
@@ -148,7 +148,7 @@ grails.project.dependency.resolution = {
         compile ':spring-security-ldap:1.0.6'
         compile ':spring-security-shibboleth-native-sp:1.0.3'
 
-        runtime ":gsp-resources:0.4.4"
+        // runtime ":gsp-resources:0.4.4"
         runtime ':jquery:1.11.0.2'
 
         runtime ":audit-logging:1.0.3"
